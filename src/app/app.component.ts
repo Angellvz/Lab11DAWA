e37a000acd36b4300ec24812af1b8261bd96357f
luis.ventura@tecsup.edu.pe <102069450+Angellvz@users.noreply.github.com>
2023-05-31 11:15:06 -0500

    v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+tasks: string[] = [];
+
+addTask(task: string) {
+this.tasks.push(task);
+}
 }
