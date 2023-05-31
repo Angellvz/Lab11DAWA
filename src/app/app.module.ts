639d80bd54d6cc25826a770086594cf1e3d4d6d1
luis.ventura@tecsup.edu.pe <102069450+Angellvz@users.noreply.github.com>
2023-05-31 11:48:30 -0500

    v0.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFilterComponent } from './task-filter/task-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTaskComponent,
-    TaskListComponent
+    TaskListComponent,
+    TaskFilterComponent
   ],
   imports: [
     BrowserModule,
